8049edb02ba7b24baad7b64e1e26304895bbb57b
enjoys <deadly.info@gmail.com>
2021-08-01 19:17:36 +0300

    remove CSRF Rule. Replaced on Callback Rule. crypt() & hash_equals() replaced on password_hash() & password_verify()

--- tests/Elements/CsrfTest.php
+++ tests/Elements/CsrfTest.php
@@ -4,7 +4,10 @@ namespace Tests\Enjoys\Forms\Elements;
 
 use Enjoys\Forms\Elements\Csrf;
 use Enjoys\Forms\Form;
+use Enjoys\Forms\Rule\Callback;
+use Enjoys\Http\ServerRequest;
 use Enjoys\Session\Session;
+use HttpSoft\ServerRequest\ServerRequestCreator;
 use PHPUnit\Framework\TestCase;
 
 new Session();
@@ -27,6 +30,54 @@ class CsrfTest extends TestCase
 
         $form->setMethod('get');
         $this->assertTrue(null === $form->getElement(Form::_TOKEN_CSRF_));
+    }
+
+    public function testValidateCsrfTrue()
+    {
+        $key = 'test';
+        $request = new ServerRequest(
+            ServerRequestCreator::createFromGlobals(
+                null,
+                null,
+                null,
+                null,
+                [
+                    Form::_TOKEN_CSRF_ => password_hash($key, PASSWORD_DEFAULT)
+                ],
+            )
+        );
+
+
+        $csrf = new Csrf($key);
+        $csrf->setRequest($request);
+
+        /** @var Callback $rule */
+        $rule = $csrf->getRules()[0];
+        $this->assertTrue($rule->validate($csrf));
+    }
+
+    public function testValidateCsrfFalse()
+    {
+        $key = 'test';
+        $request = new ServerRequest(
+            ServerRequestCreator::createFromGlobals(
+                null,
+                null,
+                null,
+                null,
+                [
+                    Form::_TOKEN_CSRF_ => 'invalid_token'
+                ],
+            )
+        );
+
+
+        $csrf = new Csrf($key);
+        $csrf->setRequest($request);
+
+        /** @var Callback $rule */
+        $rule = $csrf->getRules()[0];
+        $this->assertFalse($rule->validate($csrf));
     }
 
 }
