616093be94247e5bf67b31f37491dfe5e79b6d39
Enjoys <deadly.info@gmail.com>
2022-03-29 18:46:19 +0300

    added Attribute.php

---
+++ src/Attribute.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Enjoys\Forms;
+
+use Webmozart\Assert\Assert;
+
+final class Attribute
+{
+    private array $values = [];
+
+    private bool $multiple = false;
+    private string $separator = '';
+
+    public function __construct(private string $name, mixed $value = null)
+    {
+        if ($name === 'class') {
+            $this->setMultiple(true);
+        }
+
+        if ($value !== null) {
+            $this->add($value);
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('%s="%s"', $this->getName(), $this->getValueString());
+    }
+
+    public function getValues(): array
+    {
+        return $this->values;
+    }
+
+    public function getValueString(): string
+    {
+        return implode($this->separator, $this->getValues());
+    }
+
+    public function set(array $values): void
+    {
+        $this->clear();
+        foreach ($values as $item) {
+            $this->add($item);
+        }
+    }
+
+    public function clear(): void
+    {
+        $this->values = [];
+    }
+
+    public function has(mixed $value): bool
+    {
+        return in_array($value, $this->values, true);
+    }
+
+
+
+    public function add(mixed $value): void
+    {
+        $value = $this->normalize($value);
+
+        if (!$this->isMultiple()) {
+            $this->clear();
+        } else {
+            $value = explode($this->separator, $value);
+        }
+        foreach ((array)$value as $item) {
+            if (!$this->has($item)) {
+                $this->values[] = $item;
+            }
+        }
+    }
+
+    public function remove(string $value): bool
+    {
+        $key = array_search($value, $this->values, true);
+
+        if ($key === false) {
+            return false;
+        }
+
+        unset($this->values[$key]);
+
+        return true;
+    }
+
+    private function normalize(mixed $value): string
+    {
+        if ($value instanceof \Closure) {
+            $value = $value();
+        }
+
+        Assert::scalar($value);
+
+        return (string)$value;
+    }
+
+
+    public function isMultiple(): bool
+    {
+        return $this->multiple;
+    }
+
+
+    public function setMultiple(bool $multiple, string $separator = ' '): void
+    {
+        $this->multiple = $multiple;
+        $this->separator = $separator;
+    }
+}
---
+++ tests/AttributeTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Enjoys\Forms;
+
+use Enjoys\Forms\Attribute;
+use PHPUnit\Framework\TestCase;
+
+class AttributeTest extends TestCase
+{
+    public function testAddAttributeNotMultiple()
+    {
+        $attr = new Attribute('id', 'my id with space');
+        $this->assertCount(1, $attr->getValues());
+        $this->assertSame('id="my id with space"', $attr->__toString());
+        $attr->add(1);
+        $this->assertCount(1, $attr->getValues());
+        $this->assertSame('id="1"', $attr->__toString());
+    }
+
+    public function testAddAttributeAutoMultiple()
+    {
+        $attr = new Attribute('class', 'many classes defined by space');
+        $this->assertCount(5, $attr->getValues());
+        $this->assertSame('class="many classes defined by space"', $attr->__toString());
+        $attr->add('more class');
+        $this->assertCount(7, $attr->getValues());
+        $this->assertSame('class="many classes defined by space more class"', $attr->__toString());
+        $attr->add('repeat more class');
+        $this->assertCount(8, $attr->getValues());
+        $this->assertSame('class="many classes defined by space more class repeat"', $attr->__toString());
+    }
+
+    public function testAddAttributeAutoMultipleWithSetSeparator()
+    {
+        $attr = new Attribute('class', 'many classes defined by space');
+        $attr->setMultiple(true, '-');
+        $this->assertCount(5, $attr->getValues());
+        $this->assertSame('class="many-classes-defined-by-space"', $attr->__toString());
+        $attr->add('must-exploded');
+        $this->assertCount(7, $attr->getValues());
+        $this->assertSame('class="many-classes-defined-by-space-must-exploded"', $attr->__toString());
+        $attr->remove('defined');
+        $attr->remove('classes');
+        $attr->remove('by');
+        $attr->add('repeat must be division by space');
+        $this->assertCount(5, $attr->getValues());
+        $this->assertSame('class="many-space-must-exploded-repeat must be division by space"', $attr->__toString());
+    }
+}
