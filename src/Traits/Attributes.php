ee60a7b081509da538cc4d0234de5042fc5140fa
enjoys <deadly.info@gmail.com>
2022-04-02 18:38:31 +0300

    Traits/Attributes.php coverage score 100%

--- src/Traits/Attributes.php
+++ src/Traits/Attributes.php
@@ -7,9 +7,6 @@ namespace Enjoys\Forms\Traits;
 use Enjoys\Forms\Attribute;
 use Enjoys\Forms\AttributeCollection;
 
-/**
- * @author Enjoys
- */
 trait Attributes
 {
 
@@ -43,7 +40,7 @@ trait Attributes
     }
 
     /**
-     * @param array $attributes
+     * @param Attribute[] $attributes
      * @param string $namespace
      * @return $this
      */
@@ -97,89 +94,15 @@ trait Attributes
         return $this;
     }
 
-//
-//    public function setAttributes(array $attributes, string $namespace = 'general'): self
-//    {
-//        foreach ($attributes as $key => $value) {
-//            if (is_array($value)) {
-//                $this->setAttribute($key, implode(" ", $value), $namespace);
-//                continue;
-//            }
-//            if (is_int($key)) {
-//                $this->setAttribute($value, null, $namespace);
-//                continue;
-//            }
-//            $this->setAttribute($key, $value, $namespace);
-//        }
-//        return $this;
-//    }
-
-//
-//    public function setAttribute(string $name, $value = null, string $namespace = 'general'): self
-//    {
-//        $name = \trim($name);
-//
-//        $this->getAttributeCollection($namespace)->add(new Attribute($name, $value));
-//
-//        if ($value instanceof \Closure) {
-//            $value = $value();
-//            Assert::nullOrString($value);
-//        }
-//
-//        if ($value === null) {
-//            $this->attributes[$namespace][$name] = null;
-//            return $this;
-//        }
-//
-//        if (!is_string($value) && is_scalar($value)) {
-//            $value = (string)$value;
-//        }
-//
-//
-//        $value = htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE);
-//
-//        if ($name === 'class') {
-//            $classList = $this->getClassesList($namespace);
-//
-//            if (!in_array($value, $classList)) {
-//                $value = implode(" ", array_merge($classList, [$value]));
-//            }
-//        }
-//
-//        $this->attributes[$namespace][$name] = $value;
-//
-////        if ($name == 'name' && $this->attributes[$namespace]['name'] !== $value) {
-////            $this->setName($value);
-////        }
-//
-//
-//        return $this;
-//    }
-
-//    public function getAttr(string $key, string $namespace = 'general'): string|null|false
-//    {
-//        if (!isset($this->attributes[$namespace])) {
-//            $this->attributes[$namespace] = [];
-//        }
-//        if (array_key_exists($key, $this->attributes[$namespace])) {
-//            return $this->attributes[$namespace][$key];
-//        }
-//        return false;
-//    }
-
-//    /**
-//     *
-//     * @param string $namespace
-//     * @return array
-//     */
-//    public function getAttrs(string $namespace = 'general'): array
-//    {
-//        if (!isset($this->attributes[$namespace])) {
-//            $this->attributes[$namespace] = [];
-//        }
-//
-//        return $this->attributes[$namespace];
-//    }
+    /**
+     * @see getAttributeCollection
+     * @param string $namespace
+     * @return AttributeCollection
+     */
+    public function getAttrs(string $namespace = 'general'): AttributeCollection
+    {
+        return $this->getAttributeCollection($namespace);
+    }
 
     /**
      *
@@ -195,37 +118,18 @@ trait Attributes
         }
 
         return ' ' . $attributesStringed;
-//        $str = [];
-//        if (!array_key_exists($namespace, $this->attributes)) {
-//            $this->attributes[$namespace] = [];
-//        }
-//        foreach ($this->attributes[$namespace] as $key => $value) {
-//            if (is_array($value)) {
-//                if (empty($value)) {
-//                    continue;
-//                }
-//                $str[] = " $key=\"" . \implode(" ", $value) . "\"";
-//                continue;
-//            }
-//
-//            if (is_null($value)) {
-//                $str[] = " $key";
-//                continue;
-//            }
-//
-//
-//            $str[] = " $key=\"$value\"";
-//        }
-//        return implode("", $str);
     }
 
-//    public function removeAttribute(string $key, string $namespace = 'general'): self
-//    {
-//        if (array_key_exists($key, $this->attributes[$namespace])) {
-//            unset($this->attributes[$namespace][$key]);
-//        }
-//        return $this;
-//    }
+    /**
+     * @param string|Attribute $attribute
+     * @param string $namespace
+     * @return $this
+     */
+    public function removeAttr(string|Attribute $attribute, string $namespace = 'general')
+    {
+        $this->getAttributeCollection($namespace)->remove($attribute);
+        return $this;
+    }
 
     public function addClass(mixed $class, string $namespace = 'general'): self
     {
@@ -238,39 +142,33 @@ trait Attributes
         $attr->add($class);
         return $this;
     }
-//
-//    public function addClasses(array $classes, string $namespace = 'general'): self
-//    {
-//        foreach ($classes as $class) {
-//            $this->addClass($class, $namespace);
-//        }
-//        return $this;
-//    }
-//
-//
-//    public function getClassesList(string $namespace = 'general'): array
-//    {
-//        $classesString = $this->getAttr('class', $namespace);
-//        if ($classesString === false || $classesString === null) {
-//            return [];
-//        }
-//        return explode(" ", $classesString);
-//    }
-//
-//    public function removeClass(string $classValue, string $namespace = 'general'): self
-//    {
-//        if (!array_key_exists('class', $this->attributes[$namespace])) {
-//            return $this;
-//        }
-//
-//        $classesList = $this->getClassesList($namespace);
-//
-//        if (false !== $key = array_search($classValue, $classesList)) {
-//            unset($classesList[$key]);
-//        }
-//
-//        $this->attributes[$namespace]['class'] = implode(" ", $classesList);
-//
-//        return $this;
-//    }
+
+    /**
+     * @param array $classes
+     * @param string $namespace
+     * @return $this
+     */
+    public function addClasses(array $classes, string $namespace = 'general')
+    {
+        foreach ($classes as $class) {
+            $this->addClass($class, $namespace);
+        }
+        return $this;
+    }
+
+    public function getClassesList(string $namespace = 'general'): array
+    {
+        return  $this->getAttr('class', $namespace)?->getValues() ?? [];
+    }
+
+    /**
+     * @param string $classValue
+     * @param string $namespace
+     * @return $this
+     */
+    public function removeClass(string $classValue, string $namespace = 'general')
+    {
+        $this->getAttr('class', $namespace)?->remove($classValue);
+        return $this;
+    }
 }
--- tests/Traits/AttributeTest.php
+++ tests/Traits/AttributeTest.php
@@ -3,178 +3,180 @@
 namespace Tests\Enjoys\Forms\Traits;
 
 use Enjoys\Forms\Attribute;
-use Enjoys\Forms\Elements\Text;
+use Enjoys\Forms\Traits\Attributes;
+use PHPUnit\Framework\TestCase;
 
-class AttributeTest
+class AttributeTest extends TestCase
 {
 
 
-    public function testAttributesTrait_addAttrs_addAttr_setAttrs_setAttr()
+    public function testAttributesTrait_addAttrs_addAttr_setAttrs_setAttr(): void
     {
-        $text = new Text('test');
-        $this->assertSame('id="test" name="test"', (string)$text->getAttributeCollection());
-        $text->addAttrs([
+        /** @var Attributes $traitAttributes */
+        $traitAttributes = $this->getMockForTrait(Attributes::class);
+        $traitAttributes->addAttrs([
             new Attribute('id', 'id'),
             new Attribute('class', 'one two'),
         ]);
-        $this->assertSame('id="test" name="test" class="one two"', (string)$text->getAttributeCollection());
-        $text->setAttrsWithClear([
+        $this->assertSame('id="id" class="one two"', (string)$traitAttributes->getAttributeCollection());
+        $traitAttributes->setAttrsWithClear([
             new Attribute('id', 'id')
         ]);
-        $this->assertSame('id="id"', (string)$text->getAttributeCollection());
-        $text->addAttr(new Attribute('id', 'newid'));
-        $text->addAttr(new Attribute('disabled'));
-        $this->assertSame('id="id" disabled', (string)$text->getAttributeCollection());
-        $text->setAttr(new Attribute('id', 'newid'));
-        $text->setAttr(new Attribute('class'));
-        $this->assertSame('disabled id="newid"', (string)$text->getAttributeCollection());
+        $this->assertSame('id="id"', (string)$traitAttributes->getAttributeCollection());
+        $traitAttributes->addAttr(new Attribute('id', 'newid'));
+        $traitAttributes->addAttr(new Attribute('disabled'));
+        $this->assertSame('id="id" disabled', (string)$traitAttributes->getAttributeCollection());
+        $traitAttributes->setAttr(new Attribute('id', 'newid'));
+        $traitAttributes->setAttr(new Attribute('class'));
+        $this->assertSame('disabled id="newid"', (string)$traitAttributes->getAttributeCollection());
     }
 
-//
-//    /**
-//     *
-//     * @var  Enjoys\Forms\Forms $obj
-//     */
-////    protected $obj;
-////
-////    protected function setUp(): void
-////    {
-////        $this->obj = new Form();
-////    }
-////
-////    protected function tearDown(): void
-////    {
-////        $this->obj = null;
-////    }
-//
-//    /**
-//     * @dataProvider data
-//     */
-//    public function test_attributes($attributes, $expect)
-//    {
-//        $trait = $this->getMockForTrait(Attributes::class);
-//        $trait->setAttrs(Attribute::createFromArray($attributes));
-//        $this->assertEquals($expect, $trait->getAttributesString());
-//    }
-//
-//    public function data()
-//    {
-//        return [
-//            [['test'], ' test'],
-//            [[54 => 'test'], ' test'],
-//            [[' 0' => 'test'], ' 0="test"'],
-//            [['test', 'id' => 'test'], ' test id="test"'],
-//            [['test' => function(){return null;}], ' test'],
-//            [['test' => function(){return 'test';}], ' test="test"'],
-//        ];
-//    }
-//
-//    public function testAddAttribute_arrays()
-//    {
-//        $trait = $this->getMockForTrait(Attributes::class);
-//        $trait->setAttrs(['first' => 'value1', 'second' => 'value2']);
-//        $this->assertSame('value1', $trait->getAttr('first'));
-//        $this->assertSame('value2', $trait->getAttr('second'));
-//
-//        $trait->setAttributes(['value_withoutkey', 'second' => 'value2']);
-//        $this->assertEquals(null, $trait->getAttr('value_withoutkey'));
-//
-//        $trait->addClass('class1');
-//        $this->assertEquals('class1', $trait->getAttr('class'));
-//
-//        $trait->addClass('class2 class3');
-//     //   $trait->addClass('class1');
-//
-//        $this->assertEquals(
-//            'class1 class2 class3',
-//            $trait->getAttr('class')
-//        );
-//
-//        $this->assertEquals(
-//            [
-//                'class1',
-//                'class2',
-//                'class3',
-//            ],
-//            $trait->getClassesList()
-//        );
-//
-//
-//        $trait->removeClass('class2');
-//        $this->assertEquals( 'class1 class3',
-//            $trait->getAttr('class')
-//        );
-//
-//
-//        $trait->setAttributes(['value_withoutkey', 'second' => 'value2'], 'extra');
-//        $trait->removeClass('class1', 'extra');
-//
-//        $trait->addClass('class1', 'extra');
-//        $trait->removeClass('class1', 'extra');
-//
-//        $this->assertEquals('', $trait->getAttr('class', 'extra'));
-//
-//        $this->assertEquals(null, $trait->getAttr('value_withoutkey'));
-//
-//        $this->assertStringContainsString(
-//            'first="value1" second="value2" value_withoutkey class="class1 class3',
-//            $trait->getAttributesString()
-//        );
-//        $this->assertStringContainsString('value_withoutkey second="value2"', $trait->getAttributesString('extra'));
-//
-//        $this->assertEquals(false, $trait->getAttr('value', 'extra2'));
-//        $this->assertEquals('', $trait->getAttributesString('extra3'));
-//
-//        $trait->setAttributes(
-//            [
-//                'first' => [
-//                    'value1',
-//                    'value2'
-//                ],
-//                'second' => 'value3'
-//            ]
-//        );
-//        $this->assertSame('value1 value2', $trait->getAttr('first'));
-//
-//        $trait->setAttributes(['first' => 'value1'], 'extra4')->getAttrs(['first' => 'value2'], 'extra4');
-//        $this->assertSame('value2', $trait->getAttr('first', 'extra4'));
-//
-//        $trait->setAttributes(
-//            [
-//                'class' => [
-//                    'value1',
-//                    'value2'
-//                ]
-//            ],
-//            'extra5'
-//        );
-//        $this->assertSame('value1 value2', $trait->getAttr('class', 'extra5'));
-//    }
-//
-//    public function testAddAttribute_class()
-//    {
-//        $trait = $this->getMockForTrait(Attributes::class);
-//        $trait->setAttribute('class', 'value1')->setAttribute('class', 'value2');
-//        $this->assertSame(['value1', 'value2'], $trait->getClassesList());
-//        $this->assertSame('value1 value2', $trait->getAttr('class'));
-//
-//        $trait->addClasses(
-//            [
-//                1,
-//                2
-//            ]
-//        );
-//
-//        $this->assertSame('value1 value2 1 2', $trait->getAttr('class'));
-//    }
-//
-//    public function testGetAttributesString()
-//    {
-//        $trait = $this->getMockForTrait(Attributes::class);
-//        $trait->setAttribute('first', 'value1');
-//        $trait->setAttribute('three', 'value3');
-//        $this->assertSame(' first="value1" three="value3"', $trait->getAttributesString());
-//    }
-//
+    public function testGetAttributeCollection()
+    {
+        /** @var Attributes $traitAttributes */
+        $traitAttributes = $this->getMockForTrait(Attributes::class);
+        $traitAttributes->addAttrs([
+            new Attribute('id', 'id'),
+            new Attribute('class', 'one two'),
+        ]);
+        $this->assertSame(2, $traitAttributes->getAttributeCollection()->count());
+        $this->assertSame(2, $traitAttributes->getAttrs()->count());
+    }
+
+    public function testRemoveAttr()
+    {
+        /** @var Attributes $traitAttributes */
+        $traitAttributes = $this->getMockForTrait(Attributes::class);
+        $traitAttributes->addAttrs([
+            new Attribute('id', 'id'),
+            new Attribute('class', 'one two'),
+        ]);
+        $this->assertSame(2, $traitAttributes->getAttributeCollection()->count());
+        $traitAttributes->removeAttr('id');
+        $this->assertSame(1, $traitAttributes->getAttributeCollection()->count());
+    }
+
+    /**
+     * @dataProvider data
+     */
+    public function testTraitSetAttributes($attributes, $expect): void
+    {
+        /** @var Attributes $traitAttributes */
+        $traitAttributes = $this->getMockForTrait(Attributes::class);
+        $traitAttributes->setAttrs(Attribute::createFromArray($attributes));
+        $this->assertEquals($expect, $traitAttributes->getAttributesString());
+    }
+
+    public function data()
+    {
+        return [
+            [['test'], ' test'],
+            [[54 => 'test'], ' test'],
+            [[' 0' => 'test'], '  0="test"'],
+            [['test', 'id' => 'test'], ' test id="test"'],
+            [
+                [
+                    'test' => function () {
+                        return null;
+                    }
+                ],
+                ' test'
+            ],
+            [
+                [
+                    'test' => function () {
+                        return 'test';
+                    }
+                ],
+                ' test="test"'
+            ],
+        ];
+    }
+
+    public function testGetAttributesString()
+    {
+        /** @var Attributes $traitAttributes */
+        $traitAttributes = $this->getMockForTrait(Attributes::class);
+        $traitAttributes->setAttr(Attribute::create('first', 'value1'));
+        $traitAttributes->setAttr(Attribute::create('three', 'value3'));
+        $this->assertSame(' first="value1" three="value3"', $traitAttributes->getAttributesString());
+    }
+
+    public function testAddAttribute_class()
+    {
+        /** @var Attributes $traitAttributes */
+        $traitAttributes = $this->getMockForTrait(Attributes::class);
+        $traitAttributes->setAttr(
+            Attribute::create('class', 'value1')
+        )->getAttr('class')->add('value2');
+        $this->assertSame(['value1', 'value2'], $traitAttributes->getClassesList());
+        $this->assertSame('value1 value2', $traitAttributes->getAttr('class')->getValueString());
+
+        $traitAttributes->addClasses(
+            [
+                1,
+                2
+            ]
+        );
+
+        $this->assertSame('value1 value2 1 2', $traitAttributes->getAttr('class')->getValueString());
+    }
+
+    public function testAddAttributeArrays()
+    {
+        /** @var Attributes $traitAttributes */
+        $traitAttributes = $this->getMockForTrait(Attributes::class);
+        $traitAttributes->setAttrs(Attribute::createFromArray(['first' => 'value1', 'second' => 'value2']));
+        $this->assertSame('value1', $traitAttributes->getAttr('first')->getValueString());
+        $this->assertSame('value2', $traitAttributes->getAttr('second')->getValueString());
+
+        $traitAttributes->setAttrs(Attribute::createFromArray(['value_withoutkey', 'second' => 'value2']));
+        $this->assertSame('value_withoutkey', $traitAttributes->getAttr('value_withoutkey')->__toString());
+
+        $traitAttributes->addClass('class1');
+        $this->assertSame('class1', $traitAttributes->getAttr('class')->getValueString());
+
+        $traitAttributes->addClass('class2 class3');
+     //   $trait->addClass('class1');
+
+        $this->assertSame(
+            'class1 class2 class3',
+            $traitAttributes->getAttr('class')->getValueString()
+        );
+
+        $this->assertSame(
+            [
+                'class1',
+                'class2',
+                'class3',
+            ],
+            $traitAttributes->getClassesList()
+        );
+
+
+        $traitAttributes->removeClass('class2');
+        $this->assertSame( 'class1 class3',
+            $traitAttributes->getAttr('class')->getValueString()
+        );
+
+
+        $traitAttributes->setAttrs(Attribute::createFromArray(['value_withoutkey', 'second' => 'value2']), 'extra');
+
+        $traitAttributes->addClass('class1', 'extra');
+        $traitAttributes->removeClass('class1', 'extra');
+
+        $this->assertSame([],$traitAttributes->getAttr('class', 'extra')->getValues());
+
+
+        $this->assertStringContainsString(
+            'first="value1" value_withoutkey second="value2" class="class1 class3',
+            $traitAttributes->getAttributesString()
+        );
+        $this->assertStringContainsString('value_withoutkey second="value2"', $traitAttributes->getAttributesString('extra'));
+
+        $this->assertSame(null, $traitAttributes->getAttr('value', 'extra2'));
+        $this->assertSame('', $traitAttributes->getAttributesString('extra3'));
+    }
+
 
 }
