a992d0b0ab5def41831a8c0e30e5b476afa06b83
Enjoys <deadly.info@gmail.com>
2022-04-19 18:49:28 +0300

    added methods setMin setMax setStep

--- src/Elements/Range.php
+++ src/Elements/Range.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Enjoys\Forms\Elements;
 
+use Enjoys\Forms\AttributeFactory;
 use Enjoys\Forms\Element;
 use Enjoys\Forms\Interfaces\Ruled;
 use Enjoys\Forms\Traits\Description;
 use Enjoys\Forms\Traits\Rules;
+use Webmozart\Assert\Assert;
 
 class Range extends Element implements Ruled
 {
@@ -15,4 +17,33 @@ class Range extends Element implements Ruled
     use Rules;
 
     protected string $type = 'range';
+
+    public function setMin(string|int $min): self
+    {
+        Assert::numeric($min);
+        $min = (int) $min;
+        $this->setAttr(AttributeFactory::create('min', $min));
+        return $this;
+    }
+
+
+    public function setMax(int|string $max): self
+    {
+        Assert::numeric($max);
+        $max = (int) $max;
+        $this->setAttr(AttributeFactory::create('max', $max));
+        return $this;
+    }
+
+
+    /**
+     * @param numeric $step
+     * @return $this
+     */
+    public function setStep(float|int|string $step): self
+    {
+        Assert::numeric($step);
+        $this->setAttr(AttributeFactory::create('step', $step));
+        return $this;
+    }
 }
---
+++ tests/Elements/RangeTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Enjoys\Forms\Elements;
+
+use Enjoys\Forms\Elements\Range;
+use PHPUnit\Framework\TestCase;
+use Webmozart\Assert\InvalidArgumentException;
+
+class RangeTest extends TestCase
+{
+    public function testSetMax()
+    {
+        $el = new Range('name');
+        $el->setMax(10);
+        $this->assertSame('<input type="range" id="name" name="name" max="10">', $el->baseHtml());
+
+        $el->setMax('8');
+        $this->assertSame('<input type="range" id="name" name="name" max="8">', $el->baseHtml());
+
+        $el->setMax('9.5');
+        $this->assertSame('<input type="range" id="name" name="name" max="9">', $el->baseHtml());
+
+        $this->expectException(InvalidArgumentException::class);
+        $el->setMax('invalid');
+    }
+
+    public function testSetStep()
+    {
+        $el = new Range('name');
+        $el->setStep(10);
+        $this->assertSame('<input type="range" id="name" name="name" step="10">', $el->baseHtml());
+
+        $el->setStep(.1);
+        $this->assertSame('<input type="range" id="name" name="name" step="0.1">', $el->baseHtml());
+
+        $el->setStep('8');
+        $this->assertSame('<input type="range" id="name" name="name" step="8">', $el->baseHtml());
+
+        $el->setStep('9.5');
+        $this->assertSame('<input type="range" id="name" name="name" step="9.5">', $el->baseHtml());
+
+        $this->expectException(InvalidArgumentException::class);
+        $el->setStep('invalid');
+    }
+
+    public function testSetMin()
+    {
+        $el = new Range('name');
+        $el->setMin(10);
+        $this->assertSame('<input type="range" id="name" name="name" min="10">', $el->baseHtml());
+
+        $el->setMin('8');
+        $this->assertSame('<input type="range" id="name" name="name" min="8">', $el->baseHtml());
+
+        $el->setMin('9.5');
+        $this->assertSame('<input type="range" id="name" name="name" min="9">', $el->baseHtml());
+
+        $this->expectException(InvalidArgumentException::class);
+        $el->setMin('invalid');
+    }
+}
