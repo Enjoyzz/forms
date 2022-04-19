712d9a55fb5ef5e43704957cbf693864a6c6d99b
Enjoys <deadly.info@gmail.com>
2022-04-19 18:49:38 +0300

    added methods setMin setMax

--- src/Elements/Date.php
+++ src/Elements/Date.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Enjoys\Forms\Elements;
 
+use Enjoys\Forms\AttributeFactory;
 use Enjoys\Forms\Element;
 use Enjoys\Forms\Interfaces\Ruled;
 use Enjoys\Forms\Traits\Description;
@@ -15,4 +16,16 @@ class Date extends Element implements Ruled
     use Rules;
 
     protected string $type = 'date';
+
+    public function setMin(\DateTimeInterface $date): self
+    {
+        $this->setAttr(AttributeFactory::create('min', $date->format('Y-m-d')));
+        return $this;
+    }
+
+    public function setMax(\DateTimeInterface $date): self
+    {
+        $this->setAttr(AttributeFactory::create('max', $date->format('Y-m-d')));
+        return $this;
+    }
 }
