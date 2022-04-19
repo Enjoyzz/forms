da6baa9b3d07f137f5c4ecb701ea7cbbac9c317d
Enjoys <deadly.info@gmail.com>
2022-04-19 16:01:03 +0300

    added methods setMultiple

--- src/Elements/Email.php
+++ src/Elements/Email.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Enjoys\Forms\Elements;
 
+use Enjoys\Forms\AttributeFactory;
 use Enjoys\Forms\Element;
 use Enjoys\Forms\Interfaces\Ruled;
 use Enjoys\Forms\Traits\Description;
@@ -15,4 +16,10 @@ class Email extends Element implements Ruled
     use Rules;
 
     protected string $type = 'email';
+
+    public function setMultiple(): self
+    {
+        $this->setAttr(AttributeFactory::create('multiple'));
+        return $this;
+    }
 }
