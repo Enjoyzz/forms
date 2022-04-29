ff181f7bebd72bc6921e2774e55c27fa306fe7f2
Enjoys <deadly.info@gmail.com>
2022-04-29 14:12:05 +0300

    added testFluentDescription()

--- tests/Traits/DescriptionTest.php
+++ tests/Traits/DescriptionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Enjoys\Forms\Traits;
 
+use Enjoys\Forms\Elements\Text;
 use Enjoys\Forms\Traits\Description;
 use PHPUnit\Framework\TestCase;
 
@@ -17,5 +18,15 @@ class DescriptionTest extends TestCase
         $this->assertEquals('desc', $mock->getDescription());
         $mock->setDescription();
         $this->assertNull($mock->getDescription());
+    }
+
+    public function testFluentDescription()
+    {
+        $el = new Text('test');
+        $el->setDescription('desc')
+            ->setLabel('label')
+        ;
+        $this->assertSame('desc', $el->getDescription());
+        $this->assertSame('label', $el->getLabel());
     }
 }
