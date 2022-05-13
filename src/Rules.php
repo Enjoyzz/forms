541dce18b582b71e6f23ab653aec1a152a5e97b3
Enjoys <deadly.info@gmail.com>
2022-05-13 17:33:51 +0300

    remove class Rules

--- src/Rules.php
+++ src/Rules.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Enjoys\Forms;
 
 use Enjoys\Forms\Rule;
-use Enjoys\Forms\Traits\Request;
 
-class Rules
+interface Rules
 {
-    use Request;
 
     public const CALLBACK = Rule\Callback::class;
     public const CAPTCHA = Rule\Captcha::class;
@@ -20,50 +18,4 @@ class Rules
     public const REQUIRED = Rule\Required::class;
     public const UPLOAD = Rule\Upload::class;
 
-
-    private ?string $message;
-    private array $params = [];
-
-    public function __construct(?string $message = null, mixed $params = [])
-    {
-        $this->setParams($params);
-        $this->message = $this->setMessage($message);
-    }
-
-    public function setParams(mixed $params): void
-    {
-
-        if (is_array($params)) {
-            $this->params = $params;
-            return;
-        }
-        $this->params[] = $params;
-    }
-
-    public function getParams(): array
-    {
-        return $this->params;
-    }
-
-    /**
-     * @param array-key $key
-     * @return mixed|null
-     */
-    public function getParam(int|string $key): mixed
-    {
-        if (isset($this->params[$key])) {
-            return $this->params[$key];
-        }
-        return null;
-    }
-
-    public function setMessage(?string $message = null): ?string
-    {
-        return $this->message = $message;
-    }
-
-    public function getMessage(): ?string
-    {
-        return $this->message;
-    }
 }
