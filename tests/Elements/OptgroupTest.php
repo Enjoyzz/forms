0b6e6e457da3a6d55028ea672fbd798fcc1ca0b3
enjoys <deadly.info@gmail.com>
2024-06-02 01:53:23 +0300

    added new tests for opt group

--- tests/Elements/OptgroupTest.php
+++ tests/Elements/OptgroupTest.php
@@ -37,19 +37,53 @@ class OptgroupTest extends _TestCase
         $form = new Form();
         $form->setDefaults([
             'select1' => [
-                1,3,'b','c'
+                1,
+                3,
+                'b',
+                'c'
             ]
         ]);
         $select = $form->select('select1')
-                ->setOptgroup('numbers', [1,2,3], [], true)
-                ->setOptgroup('alpha', ['a', 'b', 'c'], [], true)
-                ->setMultiple()
-                ;
+            ->setOptgroup('numbers', [1, 2, 3], [], true)
+            ->setOptgroup('alpha', ['a', 'b', 'c'], [], true)
+            ->setMultiple();
         $this->assertNotNull($select->getElements()[0]->getElements()[0]->getAttribute('selected'));
         $this->assertNull($select->getElements()[0]->getElements()[1]->getAttribute('selected'));
         $this->assertNotNull($select->getElements()[0]->getElements()[2]->getAttribute('selected'));
         $this->assertNull($select->getElements()[1]->getElements()[0]->getAttribute('selected'));
         $this->assertNotNull($select->getElements()[1]->getElements()[1]->getAttribute('selected'));
         $this->assertNotNull($select->getElements()[1]->getElements()[2]->getAttribute('selected'));
+    }
+
+    public function testSetDefaultsNoMultiple()
+    {
+        $form = new Form();
+        $form->setDefaults([
+            'select1' => 2
+        ]);
+        $select = $form->select('select1')
+            ->setOptgroup('numbers', ['opt1', 'opt2', 'opt3'])
+            ->setOptgroup('alpha', ['opt4', 'opt5', 'opt6']);
+
+        $this->assertNotNull($select->getElements()[0]->getElements()[2]->getAttribute('selected'));
+        $this->assertSame('opt3', $select->getElements()[0]->getElements()[2]->getLabel());
+    }
+
+    public function testSetDefaultsNoMultipleWhenNameIsArray()
+    {
+        $form = new Form();
+        $form->setDefaults([
+            's' => [
+                's1' => [
+                    's2' => '500'
+                ]
+            ]
+        ]);
+        $select = $form->select('s[s1][s2]')
+            ->fill(['null'])
+            ->setOptgroup('numbers', [300 => 'opt1', 500 => 'opt2', 800 => 'opt3'])
+            ->setOptgroup('alpha', [350 => 'opt4', 548 => 'opt5', 795 => 'opt6']);
+        $this->assertNotNull($select->getElements()[1]->getElements()[1]->getAttribute('selected'));
+        $this->assertSame('opt2', $select->getElements()[1]->getElements()[1]->getLabel());
     }
 }
